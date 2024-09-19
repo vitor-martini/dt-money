2851ec99a5fdfbc5a4f08029abf05db9be2f8b20
Vitor Martini <vitor-martini@outlook.com>
2024-09-18 22:36:02 -0300

    feat: modal component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
       outline: 0;
-      box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']}
+      box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
   }
 
   body {
